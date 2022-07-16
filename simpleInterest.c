0d14f9f0ffc34be0531050331e2479c0dab7b1a2
jayrajderasari <63353298+jayrajderasari@users.noreply.github.com>
2022-07-16 22:55:25 +0530

    Add day-1 basic programs

---
+++ simpleInterest.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+	int principal, rate, time, interest;
+   printf("Enter Principal Amount:");
+   scanf("%d", &principal);
+   printf("Enter Interest Rate:");
+   scanf("%d", &rate);
+   printf("Enter Time Period:");
+   scanf("%d", &time);
+
+   interest = (principal*rate*time)/100
+    printf("Interest: %d",interest);
+    return 0;
+}
---
+++ variableOutput.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    int age = 10, weight = 50;
+    char section = 'A';
+    float height = 5.5;
+
+    printf("Age:%d \n",age);
+    printf("Weight:%d \n",weight);
+    printf("Section:%c \n",section);
+    printf("Height:%f",height);
+    return 0;
+}
